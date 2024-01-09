3d8222941a428fdaf4915ec512e81a10d5cffdd2
wansom <ochiengwarren10@gmail.com>
2024-01-09 10:00:23 +0300

    complete,article pages

--- src/app/blog/[id]/page.js
+++ src/app/blog/[id]/page.js
@@ -2,39 +2,70 @@
 import Navbar from "@/app/components/navbar";
 import{ useEffect, useState } from 'react';
 import { useParams } from 'next/navigation'
+import '../../blog.css'
+import BlogAside from "@/app/components/blog-aside";
 
 const BlogPost = () => {
 
-  const slug  = useParams();
+  const {id}  = useParams();
   const [post, setPost] = useState(null);
-
-  // Move the useRouter outside of useEffect
-  const fetchPost = async () => {
-    try {
-      const response = await fetch(`https://intelliverseai.com/wp/wp-json/wp/v2/posts?slug=${slug}`);
-      
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-  
-      const fetchedPost = await response.json();
-      console.log('WordPress API Response:', fetchedPost);
-      setPost(fetchedPost);
-    } catch (error) {
-      console.error('Error fetching WordPress post:', error.message);
-    }
-  };
-  
-
+  const [posts, setPosts] = useState([]);
   useEffect(() => {
-       fetchPost()
-    
+    fetch("https://intelliverseai.com/wp/wp-json/wp/v2/posts")
+      .then((response) => response.json())
+      .then((posts) => {
+        const promises = posts.map((post) => {
+          return fetch(
+            `https://intelliverseai.com/wp/wp-json/wp/v2/media/${post.featured_media}`
+          )
+            .then((response) => response.json())
+            .then((media) => {
+              post.featured_image_url = media.source_url;
+              return post;
+            });
+        });
+        return Promise.all(promises);
+      })
+      .then((posts) => {
+        setPosts(posts);
+        const singlePost =posts.find((post)=>post.slug===id)
+        setPost(singlePost)
+      })
+      .catch((error) => console.error(error));
   }, []);
 
+if(!post){
+  return(
+    <p>Loading...</p>
+  )
+}
   return (
     <main className="overflow-x-hidden">
       <Navbar />
-      {/* Render the rest of your component using the 'post' data */}
+      <article>
+        <section class="section recent-post" id="recent" aria-labelledby="recent-label">
+          <div class="container">
+
+            <div class="post-main">
+                 <figure class="card-banner img-holder h-[258px] rounded-lg" >
+                        <img src={post.featured_image_url}
+                          alt={post.title.rendered} width="271" height="258" loading="lazy"
+                          class="img-cover" />
+                      </figure>
+                      <h3 class="text-black headline-3 card-title">
+                          <a href={'/blog/'+post.slug} class="link hover-2">{post.title.rendered}</a>
+                        </h3>
+
+                        <div class="card-text" dangerouslySetInnerHTML={{ __html: post.content.rendered }}></div>
+
+            </div>
+
+<BlogAside posts={posts}/>
+
+          </div>
+        </section>
+
+      </article>
     </main>
   );
 };
---
+++ src/app/components/blog-aside.jsx
@@ -0,0 +1,50 @@
+const BlogAside = ({posts}) => {
+    return ( 
+        <div class="post-aside grid-list">
+
+        <div class="card aside-card">
+
+          <h3 class=" headline-2 aside-title">
+            <span class="span text-white">Popular Posts</span>
+          </h3>
+
+          <ul class="popular-list">
+
+
+            {posts.map((post) => (
+              <li>
+                <div class="popular-card">
+
+                  <figure class="card-banner img-holder" style={{ width: '64px', height: '64px' }}>
+                    <img src={post.featured_image_url}
+                    alt={post.title.rendered} width="64" height="64" loading="lazy"
+                      class="img-cover" />
+                  </figure>
+
+                  <div class="card-content">
+
+                    <h4 class="headline headline-4 card-title">
+                      <a href={'/blog/'+post.slug} class="link hover-2 text-white">{post.title.rendered}</a>
+                    </h4>
+
+                    <div class="warpper">
+                      <p class="card-subtitle">15 mins read</p>
+
+                      <time class="publish-date" datetime="2022-04-15">15 April 2022</time>
+                    </div>
+
+                  </div>
+
+                </div>
+              </li>
+            ))}
+
+          </ul>
+
+        </div>
+
+      </div>
+     );
+}
+ 
+export default BlogAside;
