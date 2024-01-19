28d8f04035e0efa22ffdd2e373b1ac96e0644be1
wansom <ochiengwarren10@gmail.com>
2024-01-19 09:25:20 +0300

    chat categories

--- src/app/dashboard/feedback/page.js
+++ src/app/dashboard/feedback/page.js
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import Drawer from "@/app/components/drawer";
 import Image from "next/image";
 import { useState } from "react";
@@ -6,140 +6,113 @@ import { useState } from "react";
 const DashboardFeedback = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="my-2 w-full px-10 space-y-2">
-      <Drawer isOpen={isOpen} setIsOpen={setIsOpen}>
-        <div className="px-4 border-gray-100 border-2 border-solid my-8 mx-10 w-[75%] h-fit text-14 text-black rounded-lg">
-          Occaecati est et illo quibusdam accusamus qui. Incidunt aut et molestiae ut facere aut. Est quidem iusto praesentium excepturi harum nihil tenetur facilis. Ut omnis voluptates nihil accusantium doloribus eaque debitis.
+    <div className="m-3 bg-gray-200 rounded-lg shadow-sm  w-[96%] flex gap-3">
+      <div className="basis-1/5 h-screen  px-5 py-10 space-y-5">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center text-[#2D3B79] text-10 gap-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21.75 9v.906a2.25 2.25 0 0 1-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 0 0 1.183 1.981l6.478 3.488m8.839 2.51-4.66-2.51m0 0-1.023-.55a2.25 2.25 0 0 0-2.134 0l-1.022.55m0 0-4.661 2.51m16.5 1.615a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V8.844a2.25 2.25 0 0 1 1.183-1.981l7.5-4.039a2.25 2.25 0 0 1 2.134 0l7.5 4.039a2.25 2.25 0 0 1 1.183 1.98V19.5Z"
+              />
+            </svg>
 
+            <p>All</p>
+          </div>
+
+          <div className="rounded-full bg-[#2D3B79] h-10 w-10 text-8 flex items-center justify-center text-[#45B9FB]">
+            3
+          </div>
         </div>
-      </Drawer>
-      <div className=" shadow-lg rounded-lg h-fit w-full bg-white flex items-center p-3 gap-6">
-        <div>
-          <h2 className=" text-gray-400 text-12">Tracker ID</h2>
-          <p className="text-14 text-black font-semibold">A5067484</p>
+        <div className="flex justify-between items-center">
+          <div className="flex items-center text-gray-600 text-10 gap-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21.75 9v.906a2.25 2.25 0 0 1-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 0 0 1.183 1.981l6.478 3.488m8.839 2.51-4.66-2.51m0 0-1.023-.55a2.25 2.25 0 0 0-2.134 0l-1.022.55m0 0-4.661 2.51m16.5 1.615a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V8.844a2.25 2.25 0 0 1 1.183-1.981l7.5-4.039a2.25 2.25 0 0 1 2.134 0l7.5 4.039a2.25 2.25 0 0 1 1.183 1.98V19.5Z"
+              />
+            </svg>
+
+            <p>Unread</p>
+          </div>
+
+          <div className="rounded-full bg-gray-400 h-10 w-10 text-8 flex items-center justify-center text-gray-200">
+            3
+          </div>
         </div>
-        <hr className="h-16 w-[2px] bg-gray-200" />
-        <div className="">
-          <h2 className=" text-gray-400 text-12">Category</h2>
-          <p className="text-14 text-black font-semibold">Company Culture</p>
+        <div className="flex justify-between items-center">
+          <div className="flex items-center text-[#118D57] text-10 gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21.75 9v.906a2.25 2.25 0 0 1-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 0 0 1.183 1.981l6.478 3.488m8.839 2.51-4.66-2.51m0 0-1.023-.55a2.25 2.25 0 0 0-2.134 0l-1.022.55m0 0-4.661 2.51m16.5 1.615a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V8.844a2.25 2.25 0 0 1 1.183-1.981l7.5-4.039a2.25 2.25 0 0 1 2.134 0l7.5 4.039a2.25 2.25 0 0 1 1.183 1.98V19.5Z"
+              />
+            </svg>
+
+            <p>Replied</p>
+          </div>
+
+          <div className="rounded-full h-10 w-10 text-8 flex items-center justify-center bg-[#E4F5ED] text-[#118D57]">
+            3
+          </div>
         </div>
-        <hr className="h-16 w-[2px] bg-gray-200" />
-        <div className="">
-          <h2 className=" text-gray-400 text-12">Date</h2>
-          <p className="text-14 text-black font-semibold">{new Date().toDateString()}</p>
+        <div className="flex justify-between items-center">
+          <div className="flex items-center text-[#B71D18] text-10 gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21.75 9v.906a2.25 2.25 0 0 1-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 0 0 1.183 1.981l6.478 3.488m8.839 2.51-4.66-2.51m0 0-1.023-.55a2.25 2.25 0 0 0-2.134 0l-1.022.55m0 0-4.661 2.51m16.5 1.615a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V8.844a2.25 2.25 0 0 1 1.183-1.981l7.5-4.039a2.25 2.25 0 0 1 2.134 0l7.5 4.039a2.25 2.25 0 0 1 1.183 1.98V19.5Z"
+              />
+            </svg>
+
+            <p>Dismissed</p>
+          </div>
+
+          <div className="rounded-full h-10 w-10 text-8 flex items-center justify-center bg-[#FFE4DE] text-[#B71D18]">
+            3
+          </div>
         </div>
-        <hr className="h-16 w-[2px] bg-gray-200" />
-        <button className="flex items-center gap-1 rounded-lg py-2 px-4 bg-gray-200" onClick={() => { setIsOpen(true) }}>
-          <h2 className=" text-black text-12">View</h2>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-          </svg>
-
-        </button>
-        <button className="flex items-center gap-1 rounded-lg py-2 px-4 bg-[#E4F5ED] text-[#118D57]">
-          <h2 className="text-12">Reply</h2>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z" />
-          </svg>
-
-        </button>
-        <button className="flex items-center gap-1 rounded-lg py-2 px-4 bg-[#FFE4DE] text-[#B71D18]">
-          <h2 className="  text-12">Dismiss</h2>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z" />
-          </svg>
-
-        </button>
-
+      </div>
+      <div className="basis-2/5  h-screen  px-5 py-10 space-y-3 bg-white rounded-lg my-5">
 
       </div>
-      <div className=" shadow-lg rounded-lg h-fit w-full bg-white flex items-center p-3 gap-6">
-        <div>
-          <h2 className=" text-gray-400 text-12">Tracker ID</h2>
-          <p className="text-14 text-black font-semibold">A5067484</p>
-        </div>
-        <hr className="h-16 w-[2px] bg-gray-200" />
-        <div className="">
-          <h2 className=" text-gray-400 text-12">Category</h2>
-          <p className="text-14 text-black font-semibold">Company Culture</p>
-        </div>
-        <hr className="h-16 w-[2px] bg-gray-200" />
-        <div className="">
-          <h2 className=" text-gray-400 text-12">Date</h2>
-          <p className="text-14 text-black font-semibold">{new Date().toDateString()}</p>
-        </div>
-        <hr className="h-16 w-[2px] bg-gray-200" />
-        <button className="flex items-center gap-1 rounded-lg py-2 px-4 bg-gray-200" onClick={() => { setIsOpen(true) }}>
-          <h2 className=" text-black text-12">View</h2>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-          </svg>
+      <div className="basis-3/5  h-screen  px-5 py-10 space-y-3 bg-white rounded-lg my-5 mr-2">
 
-        </button>
-        <button className="flex items-center gap-1 rounded-lg py-2 px-4 bg-[#E4F5ED] text-[#118D57]">
-          <h2 className="text-12">Reply</h2>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z" />
-          </svg>
-
-        </button>
-        <button className="flex items-center gap-1 rounded-lg py-2 px-4 bg-[#FFE4DE] text-[#B71D18]">
-          <h2 className="  text-12">Dismiss</h2>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z" />
-          </svg>
-
-        </button>
-
-
-      </div>
-      <div className=" shadow-lg rounded-lg h-fit w-full bg-white flex items-center p-3 gap-6">
-        <div>
-          <h2 className=" text-gray-400 text-12">Tracker ID</h2>
-          <p className="text-14 text-black font-semibold">A5067484</p>
-        </div>
-        <hr className="h-16 w-[2px] bg-gray-200" />
-        <div className="">
-          <h2 className=" text-gray-400 text-12">Category</h2>
-          <p className="text-14 text-black font-semibold">Company Culture</p>
-        </div>
-        <hr className="h-16 w-[2px] bg-gray-200" />
-        <div className="">
-          <h2 className=" text-gray-400 text-12">Date</h2>
-          <p className="text-14 text-black font-semibold">{new Date().toDateString()}</p>
-        </div>
-        <hr className="h-16 w-[2px] bg-gray-200" />
-        <button className="flex items-center gap-1 rounded-lg py-2 px-4 bg-gray-200" onClick={() => { setIsOpen(true) }}>
-          <h2 className=" text-black text-12">View</h2>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-          </svg>
-
-        </button>
-        <button className="flex items-center gap-1 rounded-lg py-2 px-4 bg-[#E4F5ED] text-[#118D57]">
-          <h2 className="text-12">Reply</h2>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z" />
-          </svg>
-
-        </button>
-        <button className="flex items-center gap-1 rounded-lg py-2 px-4 bg-[#FFE4DE] text-[#B71D18]">
-          <h2 className="  text-12">Dismiss</h2>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z" />
-          </svg>
-
-        </button>
-
-
-      </div>
-
-
+</div>
     </div>
   );
-}
+};
 
 export default DashboardFeedback;
--- src/app/dashboard/surveys/page.js
+++ src/app/dashboard/surveys/page.js
@@ -57,7 +57,7 @@ const DashboardSurveys = () => {
 
 
       </div>
-      <div className=" shadow-lg rounded-lg h-fit w-full bg-white flex items-center  justify-evenly p-3">
+        <div className=" shadow-lg rounded-lg h-fit w-full bg-white flex items-center  justify-evenly p-3">
         <div>
           <h2 className=" text-gray-400 text-12">Tracker ID</h2>
           <p className="text-14 text-black font-semibold">A5067484</p>
@@ -94,52 +94,6 @@ const DashboardSurveys = () => {
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
 </svg>
-
-
-
-        </button>
-
-
-      </div>
-      <div className=" shadow-lg rounded-lg h-fit w-full bg-white flex items-center  justify-evenly p-3">
-        <div>
-          <h2 className=" text-gray-400 text-12">Tracker ID</h2>
-          <p className="text-14 text-black font-semibold">A5067484</p>
-        </div>
-        <hr className="h-16 w-[2px] bg-gray-200" />
-        <div className="">
-          <h2 className=" text-gray-400 text-12">Category</h2>
-          <p className="text-14 text-black font-semibold">Company Culture</p>
-        </div>
-        <hr className="h-16 w-[2px] bg-gray-200" />
-        <div className="">
-          <h2 className=" text-gray-400 text-12">Date</h2>
-          <p className="text-14 text-black font-semibold">{new Date().toDateString()}</p>
-        </div>
-        <hr className="h-16 w-[2px] bg-gray-200" />
-        <button className="flex items-center gap-1 rounded-lg py-2 px-4 bg-gray-200" onClick={() => { setIsOpen(true) }}>
-          <h2 className=" text-black text-12">View</h2>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-          </svg>
-
-        </button>
-        <button className="flex items-center gap-1 rounded-lg py-2 px-4 bg-[#E4F5ED] text-[#118D57]">
-          <h2 className="text-12">Edit</h2>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-</svg>
-
-
-        </button>
-        <button className="flex items-center gap-1 rounded-lg py-2 px-4 bg-[#FFE4DE] text-[#B71D18]">
-          <h2 className="  text-12">Delete</h2>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-</svg>
-
-
 
         </button>
 
