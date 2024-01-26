7791eb3383d2651afd4b0c134fe1ee2899fc96df
wansom <ochiengwarren10@gmail.com>
2024-01-26 16:30:18 +0300

    review page

--- src/app/components/dashboard/usersTable.jsx
+++ src/app/components/dashboard/usersTable.jsx
@@ -22,7 +22,7 @@ const UsersTable = ({ user, employees }) => {
             {openModal === 2 && <StartSurvey user={user} />}
           </div>
         </Drawer>
-        <section class="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5">
+        <section class="bg-gray-50 dark:bg-gray-900 py-4">
     <div class="mx-auto max-w-screen-xl px-4 lg:px-12">
         <div class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
             <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
