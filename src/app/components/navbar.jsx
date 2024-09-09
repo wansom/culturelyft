8955a2af9f0cb7f0756cc4aa143855e2d9b6c8a6
wansom <ochiengwarren10@gmail.com>
2024-09-09 15:27:14 +0300

    make register button prominent

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -7,7 +7,6 @@ import Link from 'next/link';
 import { useRouter } from "next/navigation";
 const Navbar = () => {
     const [isOpen, setOpen] = useState(false);
-    const [openDropDown,setOpenDropDown]=useState(false)
     const router =useRouter()
   
     return ( 
@@ -36,6 +35,14 @@ const Navbar = () => {
             <Link href="/#assitants">Our Training</Link>
             <Link href="/register">Become an Assistant</Link>
             <Link href="/#resources">Resources</Link>
+            <button onClick={()=>{router.push('/register')}}
+                class=" flex imtes-center justify-center gap-2  px-4 py-2 text-sm font-medium leading-5 text-center text-[#081a43] transition-colors duration-150 border-[#e58e04]  border-solid border-2 rounded-lg active:bg-[#e58e04] hover:bg-[#e58e04] hover:text-white focus:outline-none focus:shadow-outline-purple"
+              >
+               REGISTER NOW<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+</svg>
+
+              </button>
            
           </div>
           <div class="menu-bar">
