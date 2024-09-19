eceaad75153d2538d3876e025ab329f0a6895b5e
wansom <ochiengwarren10@gmail.com>
2024-09-20 00:05:02 +0300

    psp

--- src/app/page.js
+++ src/app/page.js
@@ -141,7 +141,7 @@ export default function Home() {
             <div>
               <h1 className='text-[32px] font-semibold text-white'>Evaluate and Monitor</h1>
               <p className='z-10 mb-2 text-gray-300'>All our assistants complete a 3-week sales bootcamp, equipping them with top skills and tools. With fast onboarding, your team will be ready in a week. You can monitor progress and suggest improvements to accelerate business goals.</p>
-              <Link href='https://calendly.com/culturelyft-info/30min' className='mt-10 flex items-center text-[#e58e04] flex-nowrap hover:border-solid hover:border-2 hover:border-[#e58e04] w-fit px-4'>Hire Sales Assistants <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <Link href='https://calendly.com/culturelyft-info/30min' className='mt-10 flex items-center text-[#e58e04] flex-nowrap hover:border-solid hover:border-2 hover:border-[#e58e04] w-fit px-4 gap-3'>TALK TO OUR TEAM <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
               </svg></Link>
             
